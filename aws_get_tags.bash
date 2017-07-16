1bb1c4d4087976afd36d7f1a9869e52e1003cd4c
Peterson W. Santos <petersonwsantos@users.noreply.github.com>
2017-07-16 16:01:07 -0300

    Update aws_get_tags.bash

--- aws_get_tags.bash
+++ aws_get_tags.bash
@@ -1,8 +1,13 @@
-
 #!/bin/bash
+
 INSTANCE_ID=$(curl -s -w '\n' http://169.254.169.254/latest/meta-data/instance-id/)
+
 EC2_AZ=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone)
+
 EC2_REGION="`echo \"$EC2_AZ\" | sed -e 's:\([0-9][0-9]*\)[a-z]*\$:\\1:'`" 
+
 CODE_COMMIT=$(aws ec2 describe-instances --region $EC2_REGION --instance-ids $INSTANCE_ID --query "Reservations[*].Instances[*].Tags[?Key=='codecommit'].Value" --output text )
+
 REPO_PUPPET=$(aws ec2 describe-instances --region $EC2_REGION --instance-ids $INSTANCE_ID --query "Reservations[*].Instances[*].Tags[?Key=='puppet_repo'].Value" --output text )
+
 PUPPET_ROLE=$(aws ec2 describe-instances --region $EC2_REGION --instance-ids $INSTANCE_ID --query "Reservations[*].Instances[*].Tags[?Key=='puppet_profile'].Value" --output text )
