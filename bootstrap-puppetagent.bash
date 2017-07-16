c3177e6638a633bc588bb56e490103504777ce12
Peterson W. Santos <petersonwsantos@users.noreply.github.com>
2017-07-16 16:08:02 -0300

    Update bootstrap-puppetagent.bash

--- bootstrap-puppetagent.bash
+++ bootstrap-puppetagent.bash
@@ -4,17 +4,12 @@
 sed -i s/^SELINUX=.*$/SELINUX=disabled/ /etc/selinux/config
 setenforce 0
 
-
-mkdir /scripts
-cat > /scripts/aws_get_tags.bash <<EOF
 INSTANCE_ID=$(curl -s -w '\n' http://169.254.169.254/latest/meta-data/instance-id/)
-EC2_AZ=`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone`
+EC2_AZ=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone)
 EC2_REGION="`echo \"$EC2_AZ\" | sed -e 's:\([0-9][0-9]*\)[a-z]*\$:\\1:'`" 
 CODE_COMMIT=$(aws ec2 describe-instances --region $EC2_REGION --instance-ids $INSTANCE_ID --query "Reservations[*].Instances[*].Tags[?Key=='codecommit'].Value" --output text )
 REPO_PUPPET=$(aws ec2 describe-instances --region $EC2_REGION --instance-ids $INSTANCE_ID --query "Reservations[*].Instances[*].Tags[?Key=='puppet_repo'].Value" --output text )
 PUPPET_ROLE=$(aws ec2 describe-instances --region $EC2_REGION --instance-ids $INSTANCE_ID --query "Reservations[*].Instances[*].Tags[?Key=='puppet_profile'].Value" --output text )
-EOF
-bash /scripts/aws_get_tags.bash
 
 yum install -y  http://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
 yum install puppet-agent -y 
