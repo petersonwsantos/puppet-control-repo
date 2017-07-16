e2084ffb5a16bf89eb7880e7572847daa84ae5f0
Peterson W. Santos <petersonwsantos@users.noreply.github.com>
2017-07-16 15:12:59 -0300

    Update bootstrap-puppetagent.bash

--- bootstrap-puppetagent.bash
+++ bootstrap-puppetagent.bash
@@ -1,19 +1,24 @@
 #/bin/bash
 
 # puppet_profile  essential_linux   
+sed -i s/^SELINUX=.*$/SELINUX=disabled/ /etc/selinux/config
+setenforce 0
 
 
+mkdir /scripts
+cat > /scripts/aws_get_tags.bash <<EOF
 INSTANCE_ID=$(curl -s -w '\n' http://169.254.169.254/latest/meta-data/instance-id/)
 EC2_AZ=`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone`
 EC2_REGION="`echo \"$EC2_AZ\" | sed -e 's:\([0-9][0-9]*\)[a-z]*\$:\\1:'`" 
 CODE_COMMIT=$(aws ec2 describe-instances --region $EC2_REGION --instance-ids $INSTANCE_ID --query "Reservations[*].Instances[*].Tags[?Key=='codecommit'].Value" --output text )
 REPO_PUPPET=$(aws ec2 describe-instances --region $EC2_REGION --instance-ids $INSTANCE_ID --query "Reservations[*].Instances[*].Tags[?Key=='puppet_repo'].Value" --output text )
 PUPPET_ROLE=$(aws ec2 describe-instances --region $EC2_REGION --instance-ids $INSTANCE_ID --query "Reservations[*].Instances[*].Tags[?Key=='puppet_profile'].Value" --output text )
+EOF
+bash /scripts/aws_get_tags.bash
 
-sed -i s/^SELINUX=.*$/SELINUX=disabled/ /etc/selinux/config
-setenforce 0
 yum install -y  http://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
 yum install puppet-agent -y 
+
 cat > /etc/puppetlabs/code/hiera.yaml <<EOF
 ---
 version: 5
@@ -26,6 +31,7 @@ defaults:
   data_hash: yaml_data
   datadir: hieradata
 EOF
+
 yum update -y
 rpm -Uvh http://www.city-fan.org/ftp/contrib/yum-repo/rhel6/x86_64/city-fan.org-release-1-13.rhel6.noarch.rpm
 yum update libcurl -y ENABLEREPO=city-fan.org
